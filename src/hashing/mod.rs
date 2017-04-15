8adf8c9b52a00f5d0e3efd3d97899ae9c1f3418d
Kyle Garrison <kgarrison343@gmail.com>
2017-04-14 23:54:35 -0400

    Change Credential from Type synonym to Tuple type so I can implement traits for it.

--- src/hashing/mod.rs
+++ src/hashing/mod.rs
@@ -1,18 +1,39 @@
 use ring::pbkdf2;
+use rustc_serialize::base64::{ToBase64, Config};
+use std::convert::From;
+use std::vec::Vec;
 
 // Most of this code is taken from the ring doc comments for PBKDF2
 // with my own special sauce thrown in.
 static PBKDF2_PRF: &'static pbkdf2::PRF = &pbkdf2::HMAC_SHA256;
 const CREDENTIAL_LEN: usize = 32;
 const ITERATIONS: u32 = 100000;
-pub type Credential = [u8; CREDENTIAL_LEN];
+
+#[derive(Debug, PartialEq)]
+pub struct Credential([u8; CREDENTIAL_LEN]);
+
+impl ToBase64 for Credential {
+    fn to_base64(&self, config: Config) -> String {
+        self.0.to_base64(config)
+    }
+}
+
+impl From<Vec<u8>> for Credential {
+    fn from(vector : Vec<u8>) -> Self {
+        let mut arr = [0u8; CREDENTIAL_LEN];
+        for (place, element) in arr.iter_mut().zip(vector.iter()) {
+            *place = *element;
+        }
+        Credential(arr)
+    }
+}
 
 pub fn hash_password(username: &str, password: &str) -> Credential {
     let salt = generate_salt(username);
     hash_password_with_salt(password, salt)
 }
 
-pub fn verify_password(password_hash:Credential, username: &str, attempted_password: &str) -> bool {
+pub fn verify_password(password_hash: &Credential, username: &str, attempted_password: &str) -> bool {
     let salt = generate_salt(username);
     verify_password_with_salt(password_hash, attempted_password, salt)
 }
@@ -24,15 +45,15 @@ fn generate_salt(username: &str) -> &[u8] {
 
 fn hash_password_with_salt(password: &str, salt: &[u8])
                            -> Credential {
-    let mut hash: Credential = [0u8; CREDENTIAL_LEN];
+    let mut hash = [0u8; CREDENTIAL_LEN];
     pbkdf2::derive(PBKDF2_PRF, ITERATIONS, salt,
                    password.as_bytes(), &mut hash);
-    hash
+    Credential(hash)
 }
 
-fn verify_password_with_salt(password_hash: Credential, attempted_password: &str, salt: &[u8]) -> bool {
+fn verify_password_with_salt(password_hash: &Credential, attempted_password: &str, salt: &[u8]) -> bool {
     match pbkdf2::verify(PBKDF2_PRF, ITERATIONS, salt,
-                         attempted_password.as_bytes(), &password_hash) {
+                         attempted_password.as_bytes(), &password_hash.0) {
         Ok(_) => true,
         _ => false,
     }
@@ -52,7 +73,7 @@ mod tests {
         ];
         
         let hash: hashing::Credential = hashing::hash_password_with_salt(password, salt.as_slice());
-        let expected_hash: hashing::Credential = [213, 80, 254, 181, 41, 78, 38, 191, 40, 210, 228, 171, 199, 85, 191, 139, 64, 255, 246, 165, 82, 178, 147, 214, 237, 131, 138, 164, 99, 240, 134, 82];
+        let expected_hash = hashing::Credential([213, 80, 254, 181, 41, 78, 38, 191, 40, 210, 228, 171, 199, 85, 191, 139, 64, 255, 246, 165, 82, 178, 147, 214, 237, 131, 138, 164, 99, 240, 134, 82]);
         assert_eq!(hash, expected_hash);
     }
 
@@ -65,9 +86,9 @@ mod tests {
             0xd6, 0x26, 0x98, 0xda, 0xf4, 0xdc, 0x50, 0x52,
             0x24, 0xf2, 0x27, 0xd1, 0xfe, 0x39, 0x01, 0x8a
         ];
-        let actual_hash: hashing::Credential = [213, 80, 254, 181, 41, 78, 38, 191, 40, 210, 228, 171, 199, 85, 191, 139, 64, 255, 246, 165, 82, 178, 147, 214, 237, 131, 138, 164, 99, 240, 134, 82];
+        let actual_hash = hashing::Credential([213, 80, 254, 181, 41, 78, 38, 191, 40, 210, 228, 171, 199, 85, 191, 139, 64, 255, 246, 165, 82, 178, 147, 214, 237, 131, 138, 164, 99, 240, 134, 82]);
 
-        let success = hashing::verify_password_with_salt(actual_hash, password_attempt, salt.as_slice());
+        let success = hashing::verify_password_with_salt(&actual_hash, password_attempt, salt.as_slice());
 
         assert!(success);
     }
@@ -87,7 +108,7 @@ mod tests {
         let username = "IamGroot";
         let password = "swordfish";
 
-        let hash: hashing::Credential = [52, 80, 203, 151, 46, 207, 204, 13, 57, 63, 244, 102, 59, 111, 4, 119, 213, 24, 100, 100, 14, 22, 95, 56, 8, 139, 216, 66, 24, 48, 254, 22];
+        let hash = hashing::Credential([52, 80, 203, 151, 46, 207, 204, 13, 57, 63, 244, 102, 59, 111, 4, 119, 213, 24, 100, 100, 14, 22, 95, 56, 8, 139, 216, 66, 24, 48, 254, 22]);
         let actual_hash = hashing::hash_password(username, password);
 
         assert_eq!(hash, actual_hash);
@@ -98,9 +119,9 @@ mod tests {
         let username = "IamGroot";
         let password = "swordfish";
 
-        let actual_hash: hashing::Credential = [52, 80, 203, 151, 46, 207, 204, 13, 57, 63, 244, 102, 59, 111, 4, 119, 213, 24, 100, 100, 14, 22, 95, 56, 8, 139, 216, 66, 24, 48, 254, 22];
+        let actual_hash = hashing::Credential([52, 80, 203, 151, 46, 207, 204, 13, 57, 63, 244, 102, 59, 111, 4, 119, 213, 24, 100, 100, 14, 22, 95, 56, 8, 139, 216, 66, 24, 48, 254, 22]);
 
-        let success = hashing::verify_password(actual_hash, username, password);
+        let success = hashing::verify_password(&actual_hash, username, password);
         assert!(success);
     }
 
